6f7b2b8770c15105229593e78d2c7404a0ce9c5b
Mei Wu <25568561+projectoriented@users.noreply.github.com>
2022-04-07 10:43:08 +0200

    rm unused functions and commented code

--- bin/generate_gene_counts.py
+++ bin/generate_gene_counts.py
@@ -2,7 +2,6 @@
 
 import argparse
 import csv
-import os
 from pathlib import Path
 from collections import OrderedDict
 import re
@@ -31,32 +30,6 @@ def read_star_gene_cnts(sample: str, star: Path, strandedness: str) -> dict:
     return sample_ids
 
 
-def read_existing_gene_cnts(path_tsv):
-    """Read existing gene counts file."""
-    sample_ids = {}
-    gene_ids = {}
-    with open(path_tsv) as in_tsv:
-        for line in in_tsv:
-            if line.startswith("GeneID"):
-                samples = line.split()[1:]
-            else:
-                gene_id = line.split()[0]
-                counts = line.split()[1:]
-
-                for idx, cnt in enumerate(counts):
-                    gene_ids[gene_id] = int(cnt)
-                    sample_ids[samples[idx]] = gene_ids
-    return sample_ids
-
-
-def combine_dict(dict_1, dict_2):
-    """Combine the existing file->dict to the incoming one."""
-    master = {}
-    master.update(dict_1)
-    master.update(dict_2)
-    return master
-
-
 def transform_to_table(gene_ids_dict, outfile):
     """Transform in dictionary into tsv friendly."""
     rows = {}
@@ -82,35 +55,6 @@ def transform_to_table(gene_ids_dict, outfile):
             writer.writerow(rows[keys])
 
 
-def pad_table(d1, d2):
-    """Pad two different sized dictionaries."""
-    # take one sample from each dictionary
-    d1_sample = next(iter(d1))
-    d2_sample = next(iter(d2))
-
-    # get the genes to compare
-    d1_list = set(d1[d1_sample].keys())
-    d2_list = set(d2[d2_sample].keys())
-
-    # aggregate genes from both lists and take unique
-    master_list = set(list(d1_list) + list(d2_list))
-
-    for gene in master_list - d2_list:
-        d2[d2_sample][gene] = 0
-        d2_list.add(gene)
-
-    master_dict = combine_dict(d1, d2)
-    return master_dict
-
-
-def file_exists(path_tsv):
-    """Check if file exists."""
-    if os.path.exists(path_tsv):
-        return path_tsv
-    else:
-        return False
-
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         formatter_class=argparse.MetavarTypeHelpFormatter,
@@ -126,17 +70,6 @@ if __name__ == "__main__":
     parser.add_argument("--output", type=str, help="output tsv file name")
 
     args = parser.parse_args()
-    # file_exist = file_exists("external_geneCounts.tsv")
-    # in_dict = read_star_gene_cnts(args.sample, args.star, args.strandedness)
-    # if file_exist:
-    #     read_existing_gene_cnts(file_exist)
-    #     transform_to_table(
-    #         pad_table(in_dict, read_existing_gene_cnts(file_exist)),
-    #         args.output,
-    #     )
-    # else:
-    #     transform_to_table(in_dict, args.output)
-
     master_dict = {}
     for index, sample_id in enumerate(args.sample):
         sample_id = re.sub(r"[\[\],]", "", sample_id)
